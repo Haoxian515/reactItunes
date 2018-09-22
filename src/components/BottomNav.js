1f44d02d37e670b53c93ef4fa643fe25d43b745c
Benson Zheng <benson515@sbcglobal.net>
2018-09-21 23:25:15 -0700

    getting there

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -12,10 +12,31 @@ class BottomNav extends Component{
 				</div>
 				<div className="NowplayingTopLayer">
 					<img src="/musiclogo.png"></img>
+					<div className="not-playing">
+						Not Playing
+					</div>
+					<img className="play-button-icon" src="/play-button-icon.png"></img>
 				</div>
 
 				<div className="Navigation">
-						navigation	
+					<div className="icon-container">
+						<div className="menu-icon">
+							Library
+						</div>
+						<div className="menu-icon">
+							ForYou
+						</div>
+						<div className="menu-icon">
+							Browse
+						</div>
+						<div className="menu-icon"> 
+							Radio
+						</div>
+						<div className="menu-icon">
+							Search
+						</div>
+					</div>
+
 				</div>
 
 			</div>
---
+++ src/components/Icon.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import "../App.css"
+
+
+class Icon extends Component{
+	render(){
+		return(
+
+		)
+	}
+}
+
+export default Icon;
