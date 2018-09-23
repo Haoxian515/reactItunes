1b1f96761f1ba321216bcf45a3c8e1d7d47b4768
Benson Zheng <benson515@sbcglobal.net>
2018-09-22 22:43:13 -0700

    still testing

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import songs from "../songs.json";
 import "../App.css"
 
 				// <div class="Nowplaying">
@@ -7,23 +8,36 @@ import "../App.css"
 				// <div class="NowplayingOver">
 				// 	<img src="/musiclogo.png"></img>
 				// </div>
+					// <img className="Song"src="/musiclogo.png"></img>
+					// <img className="Song"src="/musiclogo.png"></img>
+					// <img className="Song"src="/musiclogo.png"></img>
+					// <img className="Song"src="/musiclogo.png"></img>
+					// <img className="Song"src="/musiclogo.png"></img>
+					// <img className="Song"src="/musiclogo.png"></img>
+					// <img className="Song"src="/musiclogo.png"></img>
+					// <img className="Song"src="/musiclogo.png"></img>
+					// <img className="Song"src="/musiclogo.png"></img>
+					// <img className="Song"src="/musiclogo.png"></img>
+					// <img className="Song"src="/musiclogo.png"></img>
+					// <img className="Song"src="/musiclogo.png"></img>
+					console.log(songs)
 class Menu extends Component{
 	render(){
 		return(
 			<div className="Menu">
-				<img className="Song"src="/musiclogo.png"></img>
-				<img className="Song"src="/musiclogo.png"></img>
-				<img className="Song"src="/musiclogo.png"></img>
-				<img className="Song"src="/musiclogo.png"></img>
-				<img className="Song"src="/musiclogo.png"></img>
-				<img className="Song"src="/musiclogo.png"></img>
-				<img className="Song"src="/musiclogo.png"></img>
-				<img className="Song"src="/musiclogo.png"></img>
-				<img className="Song"src="/musiclogo.png"></img>
-				<img className="Song"src="/musiclogo.png"></img>
-				<img className="Song"src="/musiclogo.png"></img>
-				<img className="Song"src="/musiclogo.png"></img>
+		        {
+		          songs.map(function(movie){
+		            return <div>
+					            <div className="li-song">
 
+						            <img src="/musiclogo.png"></img>
+						            <div>{movie.title}</div>
+						            <div>{movie.artist}</div>
+					            </div>
+					            
+			            </div>;
+		          })
+		        }
 			</div>
 		)
 	}
